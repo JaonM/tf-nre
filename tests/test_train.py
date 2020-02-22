211cb12a80475cdca215e40a2565ace365905d27
maqiang <739686359@qq.com>
2020-02-22 19:45:45 +0800

    add loss function

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-from tf_nre.train import distance_fn
+from tf_nre.train import distance_fn, locate_farthest_label
 
 
 def test_distance_fn():
@@ -9,3 +9,11 @@ def test_distance_fn():
     label = tf.constant(np.random.rand(32, 128))
     d = distance_fn(predicted, label)
     assert d.shape == (32,)
+
+
+def test_locate_farthest_label():
+    predicted = tf.constant(np.random.rand(32, 128))
+    label_emb = tf.constant(np.random.rand(19, 128))
+    predicted = locate_farthest_label(predicted, label_emb)
+    print(predicted.shape)
+    assert predicted.shape == (32, 128)
--- tf_nre/train.py
+++ tf_nre/train.py
@@ -1,5 +1,4 @@
 # training process
-# TODO loss function
 import tensorflow as tf
 
 
@@ -11,7 +10,42 @@ def loss_fn(predicted, label, label_emb):
     :param label_emb: tensor (label_size,label_dim)
     :return:
     """
-    pass
+    y = locate_label_dim(label, label_emb)
+    y_f = locate_farthest_label(predicted, label_emb)
+    l = 1 + distance_fn(predicted, y) - distance_fn(predicted, y_f)
+    return l
+
+
+def locate_label_dim(label, label_emb):
+    """
+
+    :param label: (batch_size,)
+    :param label_emb: (label_size,label_dim)
+    :return: (batch_size,label_dim)
+    """
+    label = tf.concat(list(map(lambda x: tf.expand_dims(label_emb[x], axis=0), label.numpy())), axis=0)
+    return label
+
+
+def locate_farthest_label(predicted, label_emb):
+    """
+    Find the farthest label according to distance function
+    :param predicted: (batch_size,num_filter)
+    :param label: (batch_size,)
+    :param label_emb: (label_size,label_dim)
+    :return:
+    """
+
+    def _inner_fn(_predicted, _label_emb):
+        _predicted = tf.expand_dims(_predicted, axis=0)
+        exp_label_emb = tf.expand_dims(_label_emb, axis=1)
+        dist = tf.map_fn(lambda x: distance_fn(_predicted, x), exp_label_emb)  # (label_size,1)
+        dist = tf.squeeze(dist)
+        far_label = tf.argmax(dist)
+        return _label_emb[far_label]  # (label_dim,)
+
+    output = tf.map_fn(lambda x: _inner_fn(x, label_emb), predicted)
+    return output  # (batch_size, label_dim)
 
 
 def distance_fn(predicted, label_tensor):
@@ -22,5 +56,6 @@ def distance_fn(predicted, label_tensor):
     label_dim == num_filter
     :return: (batch_size,)
     """
+    label_tensor = label_tensor / tf.norm(label_tensor, axis=1, keepdims=True)
     predicted = predicted / tf.norm(predicted, axis=1, keepdims=True)
     return tf.norm(predicted - label_tensor, axis=1)
