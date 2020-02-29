2623e379edaa1ef9716f2b3178988a750b291eb0
maqiang <739686359@qq.com>
2020-02-29 16:09:44 +0800

    reconstruct dataloader

--- tf_nre/dataloader.py
+++ tf_nre/dataloader.py
@@ -1,43 +1,48 @@
 # Parse serialized data from tf-record file
 import tensorflow as tf
 
-from tf_nre.train import MAX_LEN
 
+class DataLoader(object):
 
-def parse_single_train(example):
-    features = {
-        'text_seq': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
-        'e1_seq': tf.io.VarLenFeature(tf.int64),
-        'e2_seq': tf.io.VarLenFeature(tf.int64),
-        'rel_e1_pos': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),  # Need be changed to RaggedTensor after batching
-        'rel_e2_pos': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
-        'label': tf.io.FixedLenFeature((1,), tf.int64)
-    }
-    parsed_example = tf.io.parse_single_example(example, features)
-    return parsed_example
+    def __init__(self, max_len):
+        self.max_len = max_len
 
+    def parse_single_train(self, example):
+        features = {
+            'text_seq': tf.io.FixedLenFeature((self.max_len,), tf.int64),
+            'e1_seq': tf.io.VarLenFeature(tf.int64),
+            'e2_seq': tf.io.VarLenFeature(tf.int64),
+            'rel_e1_pos': tf.io.FixedLenFeature((self.max_len,), tf.int64),
+            # Need be changed to RaggedTensor after batching
+            'rel_e2_pos': tf.io.FixedLenFeature((self.max_len,), tf.int64),
+            'label': tf.io.FixedLenFeature((1,), tf.int64)
+        }
 
-def parse_single_test(example):
-    features = {
-        'text_seq': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
-        'e1_seq': tf.io.VarLenFeature(tf.int64),
-        'e2_seq': tf.io.VarLenFeature(tf.int32),
-        'rel_e1_pos': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
-        'rel_e2_pos': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
-    }
-    parsed_example = tf.io.parse_single_example(example, features)
-    return parsed_example
+        parsed_example = tf.io.parse_single_example(example, features)
+        return parsed_example
 
+    def parse_single_test(self, example):
 
-def get_dataset(filename, train=True):
-    dataset = tf.data.TFRecordDataset(filename)
-    if train:
-        dataset = dataset.map(parse_single_train)
-    elif not train:
-        dataset = dataset.map(parse_single_test)
-    return dataset
+        features = {
+            'text_seq': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
+            'e1_seq': tf.io.VarLenFeature(tf.int64),
+            'e2_seq': tf.io.VarLenFeature(tf.int32),
+            'rel_e1_pos': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
+            'rel_e2_pos': tf.io.FixedLenFeature((MAX_LEN,), tf.int64),
+        }
+        parsed_example = tf.io.parse_single_example(example, features)
+        return parsed_example
+
+    def get_dataset(self, filename, train=True):
+        dataset = tf.data.TFRecordDataset(filename)
+        if train:
+            dataset = dataset.map(self.parse_single_train)
+        elif not train:
+            dataset = dataset.map(self.parse_single_test)
+        return dataset
 
 
 if __name__ == '__main__':
-    dataset = get_dataset('data/input/train.tfrecord')
+    dataloader = DataLoader(100)
+    dataset = dataloader.get_dataset('data/input/train.tfrecord')
     print(dataset)
